d15e1a6f1f58f6cb8c170e5b24ce1d1f779827ae
Silvia Gutierrez Balaguer <silviagutierrezbalaguer@MBP-de-Silvia.JOOWIN>
2021-01-20 14:35:02 +0100

    not finished

--- app.js
+++ app.js
@@ -1,102 +1,58 @@
-//Generate a random number between 1 and 500
-let randomNumber = parseInt((Math.random()*100)+1);
-const submit = document.querySelector('#subt');
-const userInput = document.querySelector('#guessField');
-const guessSlot = document.querySelector('.guesses');
-const remaining = document.querySelector('.lastResult');
-const startOver = document.querySelector('.resultParas');
-const lowOrHi = document.querySelector('.lowOrHi');
-const p = document.createElement('p');
-let previousGuesses = [];
-let numGuesses = 1;
-let playGame = true;
-
-if (playGame){
-    subt.addEventListener('click', function(e){
-        e.preventDefault();
-        //Grab guess from user
-        const guess = parseInt(userInput.value);
-        validateGuess(guess);
-    });
-}
-
-function validateGuess(guess){
-    if (isNaN(guess)){
-        alert('Please enter a valid number');
-    } else if (guess < 1) {
-        alert('Please enter a number greater than 1!');
-    } else if (guess > 100){
-        alert('Please enter a number less than 500!')
-    } else {
-        //Keep record of number of attempted guesses
-        previousGuesses.push(guess);
-        //Check to see if game is over
-        if (numGuesses === 11){
-            displayGuesses(guess);
-            displayMessage(`Game Over! Number was ${randomNumber}`);
-            endGame();
-        } else {
-        //Display previous guessed numbers
-        displayGuesses(guess);
-        //Check guess and display if wrong
-        checkGuess(guess);
+const app = Vue.createApp({
+    data(){
+        return{
+            appNumber: (Math.random()*100).toFixed(),
+            userNumber: "",
+            checkMessage: "Hola",
+            guessesNumber:[],
+            remainingGuesses: 10,
+            show: false
+        }
+    },
+    methods:{
+        checkGuess(){
+            if(this.userNumber === this.appNumber){
+                this.checkMessage = "You guessed correctly!"
+                this.show = true
+            }
+            else if(this.userNumber < this.appNumber){
+                this.checkMessage = "Too low! Try again!"
+                this.guessesNumber.push(this.userNumber) 
+                this.remainingGuesses--
+            }
+            else{
+                this.checkMessage = "Too high! Try again!" 
+                this.guessesNumber.push(this.userNumber) 
+                this.remainingGuesses--
+            }
+        },
+        endGame(){
+            if(this.remainingGuesses == 0){
+                this.checkMessage = `Game Over! Number was ${this.appNumber}`
+                this.show = true
+            }
         }
     }
-}
+})
 
-function checkGuess(guess){
-    //Display clue if guess is too high or too low
-    if (guess === randomNumber){
-        displayMessage(`You guessed correctly!`);
-        endGame();
-    } else if (guess < randomNumber) {
-        displayMessage(`Too low! Try again!`);
-    } else if (guess > randomNumber) {
-        displayMessage(`Too High! Try again!`);
-    }
-}
+app.mount("#wrapper")
 
-function displayGuesses(guess){
-    userInput.value = '';
-    guessSlot.innerHTML += `${guess}  `;
-    numGuesses++
-    remaining.innerHTML = `${11 - numGuesses}  `;
-}
+//Que la aplicación tenga un número random.
+//Coger el número que nos da el usuario en el input.
+//Evento click para comprobar si el número del input coincide con el de la app.
+//Si no coincide:
+   //-Dar un mensaje de si es muy alto o muy bajo.
+   //-Poner el número en un array de números intentados y mostrarlos por pantalla.
+   //-Restar 1 a guesses remaining
+//Si coincide: 
+    //-Mensaje de enhorabuena.
+    //-Botón para reiniciar el juego (resetear todos los valores.)
+//Si guesses remaining llega a 0 se termina el juego:
+    //- Mostrar por pantalla: Game Over! Number was "random number"
+    //-Botón para reiniciar el juego (resetear todos los valores.)
 
-function displayMessage(message){
-        lowOrHi.innerHTML = `<h1>${message}</h1>`
-}
 
-function endGame(){
-    //Clear user input
-    userInput.value = '';
-    //Disable user input button
-    userInput.setAttribute('disabled', '');
-    //Display Start new Game Button
-          p.classList.add('button');
-          p.innerHTML = `<h1 id="newGame">Start New Game</h1>`
-    startOver.appendChild(p);
-    playGame = false;
-    newGame();
-}
-
-function newGame(){
-    const newGameButton = document.querySelector('#newGame');
-    newGameButton.addEventListener('click', function(){
-        //Pick a new random number
-        randomNumber = parseInt((Math.random()*100)+1);
-        previousGuesses = [];
-        numGuesses = 1;
-        guessSlot.innerHTML = '';
-        lowOrHi.innerHTML = '';
-        remaining.innerHTML = `${11 - numGuesses}  `;
-        userInput.removeAttribute('disabled');
-        startOver.removeChild(p);
-        playGame = true;
-    })
-}
-//Allow to restart game with restart button
-//Change DIV to a form so it can accept the enter key
-
-//NOTES:
-//NaN != NaN
+//EXTRA:
+//-Borrar numero al apretar botón
+//-Poder apretar "enter" para hacer submit del numero.
+//-Quitar parentesis y comillas array.
